f63c6d8f7f150f7e2f970e432b75c86cb1ef6002
Danny <logiuratodanny@gmail.com>
2021-10-28 13:04:49 +0200

    Initial Commit

---
+++ PlayingCard/Supporting Filea/Pla.swift
@@ -0,0 +1,8 @@
+//
+//  Pla.swift
+//  PlayingCard
+//
+//  Created by Danny on 28.10.21.
+//
+
+import Foundation
---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Danny on 28.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+    }
+}
+
